386b44d1c80c9b691bad6d5f81f3f21ee22c64a2
RDixonCodes <rhdixon79@gmail.com>
2021-09-13 20:49:29 -0700

    delete favorites

--- server/routes/favorite.routes.js
+++
@@ -1,8 +0,0 @@
-const FavoriteController = require('../controllers/favorite.controller');
-const { authenticate } = require("../config/jwt.config");
-module.exports = function(app){
-    app.post('/api/favorites/:id/new', FavoriteController.getFavoriteProjects);
-    app.get('/api/favorites', FavoriteController.getAllFavorites);
-    app.get('/api/favorites/:id', FavoriteController.getFavorite);
-    app.delete('/api/favorites/:id/delete', authenticate, FavoriteController.deleteProject);
-}
